688b79a98b28574352ffcfcbf5d624c1f5b355b1
Tanis Kiel <tkiel12@gmail.com>
2021-05-01 08:21:29 -0700

    First non-repeating char completed

---
+++ firstNonRepeatingCharacter.js
@@ -0,0 +1,18 @@
+function firstNonRepeatingCharacter(string) {
+  let map = {};
+
+  for (let i = 0; i < string.length; i++) {
+    if (map[string[i]]) {
+      map[string[i]]++;
+    } else {
+      map[string[i]] = 1;
+    }
+  }
+  for (let j = 0; j < string.length; j++) {
+    if (map[string[j]] === 1) {
+      return j;
+    }
+  }
+
+  return -1;
+}
