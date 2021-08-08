aee4a826655e0163f244c3a2067995fe6a571a86
Tanis Kiel <tkiel12@gmail.com>
2021-08-08 13:26:49 -0700

    Palindrome Check Complete

---
+++ java/palindromeCheck.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Program {
+  public static boolean isPalindrome(String str) {
+		String reverse = "";
+		System.out.println(str.length());
+    for (int i = str.length() - 1; i >= 0; i--) {
+			reverse += str.charAt(i);
+		}
+		System.out.println(reverse);
+    return str.equals(reverse);
+  }
+}
