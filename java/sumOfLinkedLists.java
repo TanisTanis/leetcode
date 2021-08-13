a5d88daf048e0f4f6060004915bc637240f8d303
Tanis Kiel <tkiel12@gmail.com>
2021-08-12 20:29:24 -0700

    name changed

--- java/sumOfLinkedLists.java
+++ java/sumOfLinkedLists.java
@@ -1,6 +1,4 @@
-import java.util.*;
-
-class Program {
+class SumOfLinkedLists {
   // This is an input class. Do not edit.
   public static class LinkedList {
     public int value;
@@ -14,27 +12,27 @@ class Program {
 
   public LinkedList sumOfLinkedLists(LinkedList linkedListOne, LinkedList linkedListTwo) {
     int l1 = Integer.parseInt(new StringBuffer(getSum(linkedListOne, "")).reverse().toString());
-		int l2 = Integer.parseInt(new StringBuffer(getSum(linkedListTwo, "")).reverse().toString());
-		String sum = String.valueOf(l1 + l2);
+    int l2 = Integer.parseInt(new StringBuffer(getSum(linkedListTwo, "")).reverse().toString());
+    String sum = String.valueOf(l1 + l2);
 
-		LinkedList result = new LinkedList(Character.getNumericValue(sum.charAt(sum.length() - 1)));
-		LinkedList currentNode = result;
+    LinkedList result = new LinkedList(Character.getNumericValue(sum.charAt(sum.length() - 1)));
+    LinkedList currentNode = result;
 
-		for (int i = sum.length() - 2; i >= 0; i--) {
-			LinkedList newNode = new LinkedList(Character.getNumericValue(sum.charAt(i)));
-			currentNode.next = newNode;
-			currentNode = newNode;
-		}
+    for (int i = sum.length() - 2; i >= 0; i--) {
+      LinkedList newNode = new LinkedList(Character.getNumericValue(sum.charAt(i)));
+      currentNode.next = newNode;
+      currentNode = newNode;
+    }
 
-		return result;
+    return result;
   }
 
-	public static String getSum(LinkedList node, String number) {
-		number += node.value;
-		if (node.next == null) {
-			return number;
-		} else {
-			return getSum(node.next, number);
-		}
-	}
+  public static String getSum(LinkedList node, String number) {
+    number += node.value;
+    if (node.next == null) {
+      return number;
+    } else {
+      return getSum(node.next, number);
+    }
+  }
 }
