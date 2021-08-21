2f2238049dc0123bed3a647bc2dabe682761e3dd
Tanis Kiel <tkiel12@gmail.com>
2021-08-20 23:14:19 -0700

    threeSum

--- java/threeSum.java
+++ java/threeSum.java
@@ -3,26 +3,26 @@ import java.util.*;
 class ThreeSum {
   public static List<Integer[]> threeNumberSum(int[] array, int targetSum) {
     Arrays.sort(array);
-		List<Integer[]> result = new ArrayList<Integer[]>();
+    List<Integer[]> result = new ArrayList<Integer[]>();
 
-		for (int i = 0; i < array.length - 2; i++) {
-			int left = i + 1;
-			int right = array.length - 1;
+    for (int i = 0; i < array.length - 2; i++) {
+      int left = i + 1;
+      int right = array.length - 1;
 
-			while(left < right) {
-				int sum = array[i] + array[left] + array[right];
-				if (sum == targetSum) {
-					Integer[] triplet = {array[i], array[left], array[right]};
-					result.add(triplet);
-					left++;
-					right--;
-				} else if (sum > targetSum) {
-					right--;
-				} else if (sum < targetSum) {
-					left++;
-				}
-			}
-		}
+      while (left < right) {
+        int sum = array[i] + array[left] + array[right];
+        if (sum == targetSum) {
+          Integer[] triplet = { array[i], array[left], array[right] };
+          result.add(triplet);
+          left++;
+          right--;
+        } else if (sum > targetSum) {
+          right--;
+        } else if (sum < targetSum) {
+          left++;
+        }
+      }
+    }
     return result;
   }
 }
