8d970a3a6fad1b4575d11d5eea8cddafbc203f86
Tanis Kiel <tkiel12@gmail.com>
2021-08-22 19:21:01 -0700

    Single cycle check java

---
+++ java/singleCycleCheck.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class SingleCycleCheck {
+  public static boolean hasSingleCycle(int[] array) {
+
+    int count = 0;
+    int currentIndex = 0;
+    int[] indexes = new int[array.length];
+    Arrays.fill(indexes, 1);
+
+    while (count < array.length) {
+      System.out.println(currentIndex);
+      indexes[currentIndex] = 0;
+      int newIndex = currentIndex + array[currentIndex];
+      currentIndex = indexCalc(newIndex, array.length);
+      count++;
+    }
+
+    boolean allVisited = true;
+    for (int i = 0; i < indexes.length; i++) {
+      if (indexes[i] == 1) {
+        allVisited = false;
+      }
+    }
+
+    if (currentIndex == 0 && allVisited) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
+  private static int indexCalc(int index, int length) {
+    int newIndex = index % length;
+    return newIndex >= 0 ? newIndex : newIndex + length;
+  }
+}
